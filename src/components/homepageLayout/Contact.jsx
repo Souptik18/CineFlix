aae27f6ee00770a9d11074ea95e40b1f33ef5051
Souptik18 <souptiksarkar2017@gmail.com>
2024-06-02 16:22:21 +0530

    fix

--- src/components/homepageLayout/Contact.jsx
+++ src/components/homepageLayout/Contact.jsx
@@ -5,7 +5,7 @@ function Contact() {
   const darkModeEnabled = useSelector((state) => state.theme.darkModeEnabled);
   return (
     <>
-      <div className={`${darkModeEnabled ? "bg-black" : "bg-slate-400"} `}>
+      <div className={`${darkModeEnabled ? "bg-black" : "bg-blue-100"} `}>
         <div className="mx-auto max-w-7xl px-4">
           <div className="mx-auto max-w-7xl py-12 md:py-24">
             <div className="grid items-center justify-items-center gap-x-4 gap-y-10 lg:grid-cols-2">
@@ -102,12 +102,12 @@ function Contact() {
               <img
                 alt="Contact us"
                 className="hidden max-h-full w-full rounded-lg object-cover lg:block"
-                src="https://images.unsplash.com/photo-1543269664-56d93c1b41a6?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzZ8fGhhcHB5JTIwcGVvcGxlfGVufDB8fDB8fA%3D%3D&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=60"
+                src="https://img.freepik.com/free-vector/cinema-realistic-poster-with-illuminated-bucket-popcorn-drink-3d-glasses-reel-tickets-blue-background-with-tapes-vector-illustration_1284-77070.jpg?w=826&t=st=1717325463~exp=1717326063~hmac=65472baa7b4bc89d2f170cab6e855c514974ed8359216b89e99cc5dfd1a7fa24"
               />
             </div>
           </div>
         </div>
-        <div className="rounded-lg">
+        {/* <div className="rounded-lg">
           <div className="mx-auto max-w-4xl px-4 sm:px-6 lg:px-8">
             <div className="py-20">
               <div className="grid grid-cols-1 gap-x-20 gap-y-8 lg:grid-cols-2">
@@ -159,30 +159,12 @@ function Contact() {
               </div>
             </div>
           </div>
-        </div>
+        </div> */}
         <hr className="mt-6" />
         <section className="relative overflow-hidden bg-white py-8">
           <div className="container relative z-10 mx-auto px-4">
             <div className="-m-8 flex flex-wrap items-center justify-between">
-              <div className="w-auto p-8">
-                <a href="#">
-                  <div className="inline-flex items-center">
-                    <svg
-                      width="40"
-                      height="46"
-                      viewBox="0 0 50 56"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        d="M23.2732 0.2528C20.8078 1.18964 2.12023 12.2346 1.08477 13.3686C0 14.552 0 14.7493 0 27.7665C0 39.6496 0.0986153 41.1289 0.83823 42.0164C2.12023 43.5449 23.2239 55.4774 24.6538 55.5267C25.9358 55.576 46.1027 44.3832 48.2229 42.4602C49.3077 41.474 49.3077 41.3261 49.3077 27.8158C49.3077 14.3055 49.3077 14.1576 48.2229 13.1714C46.6451 11.7415 27.1192 0.450027 25.64 0.104874C24.9497 -0.0923538 23.9142 0.00625992 23.2732 0.2528ZM20.2161 21.8989C20.2161 22.4906 18.9835 23.8219 17.0111 25.3997C15.2361 26.7803 13.8061 27.9637 13.8061 28.0623C13.8061 28.1116 15.2361 29.0978 16.9618 30.2319C18.6876 31.3659 20.2655 32.6479 20.4134 33.0917C20.8078 34.0286 19.871 35.2119 18.8355 35.2119C17.8001 35.2119 9.0233 29.3936 8.67815 28.5061C8.333 27.6186 9.36846 26.5338 14.3485 22.885C17.6521 20.4196 18.4904 20.0252 19.2793 20.4196C19.7724 20.7155 20.2161 21.3565 20.2161 21.8989ZM25.6893 27.6679C23.4211 34.9161 23.0267 35.7543 22.1391 34.8668C21.7447 34.4723 22.1391 32.6479 23.6677 27.9637C26.2317 20.321 26.5275 19.6307 27.2671 20.3703C27.6123 20.7155 27.1685 22.7864 25.6893 27.6679ZM36.0932 23.2302C40.6788 26.2379 41.3198 27.0269 40.3337 28.1609C39.1503 29.5909 31.6555 35.2119 30.9159 35.2119C29.9298 35.2119 28.9436 33.8806 29.2394 33.0424C29.3874 32.6479 30.9652 31.218 32.7403 29.8867L35.9946 27.4706L32.5431 25.1532C30.6201 23.9205 29.0915 22.7371 29.0915 22.5892C29.0915 21.7509 30.2256 20.4196 30.9159 20.4196C31.3597 20.4196 33.6771 21.7016 36.0932 23.2302Z"
-                        fill="black"
-                      ></path>
-                    </svg>
-                    <span className="ml-4 text-lg font-bold">DevUI</span>
-                  </div>
-                </a>
-              </div>
+              <div className="w-auto p-8"></div>
               <div className="w-auto p-8">
                 <ul className="-m-5 flex flex-wrap items-center">
                   <li className="p-5">
